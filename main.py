e6d6e6fc7b0d99bf3d17b5715b1b9ea3064a727d
choi jiwoo <cho2.jiwoo@gmail.com>
2022-02-15 16:47:46 +0900

    Refactor logic designs

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
-from crawl import get_ranking
-from format import to_html
-
-    :param content: email 내용
+from smtp import Smtp
+from ranking import MusinsaRanking
 
 if __name__ == '__main__':
-	url = "https://search.musinsa.com/ranking/keyword"
-	send_to = os.environ['EMAIL_ADDRESS_TO']
-	
-	keyword_rank = get_ranking(url)
-	content = to_html(keyword_rank, now)
+    url = "https://search.musinsa.com/ranking/keyword"
+    sender = os.environ['EMAIL_ADDRESS_FROM']
+    sender_pw = os.environ['EMAIL_PW']
+    receiver = os.environ['EMAIL_ADDRESS_TO']
+
+    smtp = Smtp(sender, sender_pw)
+    ranking = MusinsaRanking(url).get_ranking()
+    smtp.send_mail(receiver, ranking)
