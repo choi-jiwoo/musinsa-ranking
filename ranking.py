938e8ce45432d1fc0a61124f91d14563108dd2ba
Choi Jiwoo <cho2.jiwoo@gmail.com>
2024-09-05 11:40:59 +0900

    Refactor logic of getting raking list

--- ranking.py
+++ ranking.py
@@ -2,46 +2,51 @@ from datetime import datetime
 from bs4 import BeautifulSoup as bs
 import re
 import requests
+from user_agent import generate_user_agent
 import pandas as pd
+
 
 class MusinsaRanking:
 
-    def __init__(self, url: str) -> None:
+    def __init__(self, url: str, max_count: int=20) -> None:
         self.url = url
+        self.max_count = max_count
+        self.headers = {
+            'User-Agent': generate_user_agent(),
+            'X-Requested-With': 'XMLHttpRequest',
+        }
 
     def _get_request(self) -> requests.Response:
-        res = requests.get(self.url)
+        res = requests.get(self.url, headers=self.headers)
         if res.status_code != 200:
             res.raise_for_status()
         return res
 
     def get_ranking(self) -> pd.DataFrame:
         res = self._get_request()
-        data = res.text
-        soup = bs(data, 'html.parser')
-        ranking_section = soup.find(class_='tbl_box_sranking')
-
-        # 키워드 부분
-        item_html = ranking_section.find_all('a')
-        # 순위 변동 방향 부분
-        status_html = ranking_section.find_all(class_='arrow')
-        # 순위 변동 부분
-        chng_html = ranking_section.find_all(class_='p_srank_last')
+        data = res.json()
+        item_list = data['data']['list']
 
         item = []
         status = []
         change = []
 
-        for i in range(len(status_html)):
-            item_name = item_html[i].attrs['title']
-            item.append(item_name)
-            change_direction = status_html[i].get_text()
-            status.append(change_direction)
-            status_change = chng_html[i].get_text()
-            change_num = re.sub(r'[^0-9]', '', status_change)
-            change.append(change_num)
+        for item_ in item_list[:self.max_count]:
+            item.append(item_['keyword'])
+            rank_change = item_['rankIncrement']
+            if rank_change == 0:
+                rank_change = '-'
+                change_icon = rank_change
+            elif rank_change > 0:
+                change_icon = '▲'
+            elif rank_change < 0:
+                change_icon = '▼'
+
+            status.append(change_icon)
+            change.append(rank_change)
 
         item_mapping = list(zip(item, status, change))
         ranking = pd.DataFrame(item_mapping,
                                columns=['Item', 'Status', 'Change'])
+        ranking.index += 1
         return ranking
